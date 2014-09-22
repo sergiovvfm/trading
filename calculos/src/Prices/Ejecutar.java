58f601876949c1396edbb4446e0af10d8886e534
viclosa <tillol@hotmail.com>
2014-09-22 12:04:03 +0200

    Leer datos tickers

---
+++ calculos/src/Prices/Ejecutar.java
@@ -0,0 +1,16 @@
+package Prices;
+
+public class Ejecutar {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Valores valores = new Valores();
+		valores.leerValores();
+//		System.out.println(valores.toString());
+
+	}
+
+}
---
+++ calculos/src/Prices/Valor.java
@@ -0,0 +1,54 @@
+package Prices;
+
+import java.util.Date;
+
+
+public class Valor {
+	
+	private long volumen;
+	private double precioAccion;
+	private Date fecha;
+	
+	
+	public Valor(long volumen, double precioAccion, Date fecha) {
+		this.volumen = volumen;
+		this.precioAccion = precioAccion;
+		this.fecha = fecha;
+	}
+	
+	public Valor() {
+	}
+
+	public long getVolumen() {
+		return volumen;
+	}
+
+	public void setVolumen(long volumen) {
+		this.volumen = volumen;
+	}
+
+	public double getPrecioAccion() {
+		return precioAccion;
+	}
+
+	public void setPrecioAccion(double precioAccion) {
+		this.precioAccion = precioAccion;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Precio accion: " + precioAccion + 
+			   "; Volumen: " + volumen + 
+			   "; Fecha: " + fecha;
+	}
+	
+}
